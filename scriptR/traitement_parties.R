fbe4eee34f75ea91ad3f6cba52a03d19955d2474
camjah <cam.jahel@gmail.com>
2024-04-11 13:47:06 +0200

    nouveaux fichiers
    nouveaux fichiers

---
+++ scriptR/traitement_parties.R
@@ -0,0 +1,22 @@
+# analyse des stratégies a partir des parties simulées
+
+
+rm(list = ls())
+
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+data.j <- read.csv("../data/applatJoueur_simule_complet.csv")
+
+# Par partie : capital total comparé à prélèvement total
+
+
+
+
+# Gini capital
+
+# gini prélèvement
+
+# stratégies collectives ?
+
+# comparer aux parties jouées
