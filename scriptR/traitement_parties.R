f217c989b22a0b945b1cf4b4d96f2b049d5e7f16
camjah <cam.jahel@gmail.com>
2024-06-05 17:11:58 +0200

    indiidus

--- scriptR/traitement_parties.R
+++ scriptR/traitement_parties.R
@@ -15,7 +15,7 @@ df<- read.csv("../data/applatJoueur_simule_complet.csv")
 # Essai joueurs
 real_gameSession <- read.csv("../data/gameSession_config_pluie_26.csv", sep = ";", encoding="latin1")
 
-
+### PREPARATION DES DONNEES #############
 # enlever le capital négatif...
 
 df$capitalmodif <- ifelse(df$capital < 0, 0, df$capital)
@@ -23,15 +23,13 @@ df$capitalmodif <- ifelse(df$capital < 0, 0, df$capital)
 agg_data_gini <- aggregate(cbind(capitalmodif, prelevement) ~ partie, data = df, FUN = function(x) {
   GINI = ineq(x)
 }, na.action = na.pass)
-
+colnames(agg_data_gini)<-c("partie","ginicap","giniprelev")
 # ¨pour les vraies parties
  irl_gini <-aggregate(cbind(Capital_final,Consommation_eau)~ID.partie, data=real_gameSession,FUN = function(x) {
    GINI = ineq(x)
  }, na.action = na.pass)
 
-
-# prof nap
-agg_data_prof<- aggregate(profnappe ~ partie, data = df, FUN = mean)
+ colnames(irl_gini)<-c("ID.partie","ginicap","giniprelev")
 
 
 # somme capital
@@ -40,86 +38,116 @@ irl_captot <-aggregate(Capital_final ~ID.partie, data=real_gameSession,FUN = sum
 
 # strat Culture
 agg_data_stratculture<- aggregate(stratCulture ~ partie, data = df, FUN = sum)
+irl_stratculture<-aggregate(strat_culture ~ID.partie, data=real_gameSession,FUN = sum)
+
 
 # strat Parcelle
 agg_data_stratParcelle<- aggregate(stratParcelle ~ partie, data = df, FUN = sum)
+irl_stratParcelle<- aggregate(strat_parcelle ~ ID.partie, data = real_gameSession, FUN = sum)
+
 
 # stratSeau
 agg_data_stratSeau<- aggregate(stratSeau ~ partie, data = df, FUN = sum)
-
+# irl_stratSeau <- aggregate(strat_parcelle ~ ID.partie, data = df, FUN = sum)
 
 # "stratGG"    
 agg_data_stratGG<- aggregate(stratGG ~ partie, data = df, FUN = sum)
+irl_stratGG <- aggregate(strat_gag ~ ID.partie, data = real_gameSession, FUN = sum)
+
 
 #"stratLance" 
 agg_data_stratLance<- aggregate(stratLance ~ partie, data = df, FUN = sum)
+
+
 
 #"puits"  
 
 agg_data_puits<- aggregate(puits ~ partie, data = df, FUN = sum)
 
+#"agrandissement"
+agg_data_foncier<- aggregate(agrandissement ~ partie, data = df, FUN = sum)
+
+
 #"empechement"
 agg_data_empechement<- aggregate(empechement ~ partie, data = df, FUN = sum)
+
+
+# prof nap
+agg_data_prof<- aggregate(profnappe ~ partie, data = df, FUN = mean)
+
+irl_conso <- aggregate(Consommation_eau ~ ID.partie, data = real_gameSession, FUN = sum)
+irl_pluies2<- aggregate(Pluie_annee2 ~ ID.partie, data = real_gameSession, FUN = mean)
+irl_pluies3<- aggregate(Pluie_annee3 ~ ID.partie, data = real_gameSession, FUN = mean)
+irl_profnappe <- irl_conso*0.039-irl_pluies2*0.039-irl_pluies3*0.039
+colnames(irl_profnappe)<- c("ID.partie","profnappe")
+# irl : grouper les prélèvement et pluies
+# =E2*0.039-I2*0.039
+
 
 
 # Tableau final
 tabparties<-cbind(agg_data_gini,agg_data_prof$profnappe,agg_data_capital$capital,
                   agg_data_stratculture$stratCulture,agg_data_stratParcelle$stratParcelle,
                   agg_data_stratSeau$stratSeau,agg_data_stratGG$stratGG,agg_data_stratLance$stratLance,
-                  agg_data_puits$puits,agg_data_empechement$empechement
+                  agg_data_puits$puits,agg_data_empechement$empechement,agg_data_foncier$agrandissement
                   )
 colnames(tabparties)<- c("partie","ginicap","giniprelev","profnappe","sum_cap","stratculture",
-                         "stratParcelle","stratSeau","StratGG","stratLance","puits","empechement")
+                         "stratParcelle","stratSeau","StratGG","stratLance","puits","empechement","foncier")
 
 # Tableau final irl
-tabparties_irl<-cbind(irl_gini,irl_captot$Capital_final)
-colnames(tabparties_irl)<-c("partie.irl","ginicap.irl","giniprelev.irl","sum_cap.irl")
+tabparties_irl<-cbind(irl_gini,irl_profnappe$profnappe,irl_captot$Capital_final, irl_stratculture$strat_culture, 
+                      irl_stratParcelle$strat_parcelle,irl_stratGG$strat_gag)
 
 
+colnames(tabparties_irl)<-c("partie","ginicap","giniprelev","profnappe","sum_cap","stratculture",
+                            "stratParcelle","StratGG")
 
+#tabparties<-tabparties_irl
+
+### ANALYSES  #############
 #♥Matrice de corrélation
 mcor <- cor(tabparties[,2:5], use="complete.obs")
-#png(filename = "../results/partie/init_egaux/fichier_correlation.png", width = 800, height = 600)
+png(filename = "../results/partie/irl/fichier_correlation.png", width = 800, height = 600)
 corrplot(mcor, type="upper", order="hclust", tl.col="black", tl.srt=45)
-#dev.off()
+dev.off()
 
 # GRAPHIQUE GINI PRELEV- GINI CAPITAL
-#png(filename = "../results/partie/init_egaux/ginicap_giniprelev.png", width = 800, height = 600)
+png(filename = "../results/partie/irl/ginicap_giniprelev.png", width = 800, height = 600)
 plot(x = tabparties$giniprelev, y = tabparties$ginicap, 
      xlab = "GINI des prélèvements", ylab = "GINI du capital", 
      main = "Relation entre le GINI des prélèvements et le GINI du capital",
      col = "blue", pch = 16)
-#dev.off()
+dev.off()
 
 
 # GRAPHIQUE GINI CAPITAL - tot capital partir
-#png(filename = "../results/partie/init_egaux/ginicap_VA.png", width = 800, height = 600)
+png(filename = "../results/partie/irl/ginicap_VA.png", width = 800, height = 600)
 plot(x = tabparties$ginicap, y = tabparties$sum_cap, 
      xlab = "GINI du capital", ylab = "valeur ajoutée tot", 
      main = "Relation entre le GINI du capital valeur ajoutée tot",
      col = "blue", pch = 16)
-#dev.off()
+dev.off()
 
 
 # GRAPHIQUE CAPITAL tot - prelevement
-#png(filename = "../results/partie/init_egaux/VA_profnappe.png", width = 800, height = 600)
+png(filename = "../results/partie/irl/VA_profnappe.png", width = 800, height = 600)
 plot(x = tabparties$sum_cap, y = tabparties$profnappe, 
      xlab = "sumcap", ylab = "profnappe", 
      main = "Relation entre entre valeur ajoutée tot et la profondeur de la nappe",
      col = "blue", pch = 16)
-#dev.off()
+dev.off()
 
 # GRAPHIQUE GINI prelev - prelevement
-#png(filename = "../results/partie/init_egaux/giniprelev_profnappe.png", width = 800, height = 600)
+png(filename = "../results/partie/irl/giniprelev_profnappe.png", width = 800, height = 600)
 plot(x = tabparties$giniprelev, y = tabparties$profnappe, 
      xlab = "giniprelev", ylab = "profnappe", 
      main = "Relation entre le GINI des prélèvements etla profondeur de la nappe ",
      col = "blue", pch = 16)
-#dev.off()
+dev.off()
 
 # GRAPHIQUE GINI prelev - sumcap
 
-#png(filename = "../results/partie/init_egaux/giniprelev_VA_giniVA.png", width = 800, height = 600)
+png(filename = "../results/partie/irl/giniprelev_VA_giniVA.png", width = 800, height = 600)
 
 color_palette <- colorRampPalette(c("blue", "red"))
 
@@ -134,12 +162,12 @@ par(xpd = TRUE) # Permet d'afficher la légende en dehors de la zone du graphiqu
 legend("topright", legend = seq(0, 1, length.out = 3), fill = color_palette(3), 
        title = "GINI du capital", cex = 0.8, bty = "n", inset = c(0, -0.1))
 
-#dev.off()
+dev.off()
 
 # GRAPHIQUE GINI CAPITAL - tot capital partir
 # Créer une palette de couleurs graduelle de bleu à rouge
 
-#png(filename = "../results/partie/init_egaux/giniVA_profnappe_VA.png", width = 800, height = 600)
+png(filename = "../results/partie/irl/giniVA_profnappe_VA.png", width = 800, height = 600)
 
 color_palette <- colorRampPalette(c("blue", "red"))
 # Convertir les valeurs de "ginicap" en couleurs selon la palette graduelle
@@ -154,13 +182,13 @@ plot(x = tabparties$ginicap, y = tabparties$profnappe,
 par(xpd = TRUE) # Permet d'afficher la légende en dehors de la zone du graphique
 legend("topright", legend = seq(mincap, maxcap, length.out = 5), fill = color_palette(5), 
        title = "sum_capital", cex = 0.8, bty = "n", inset = c(0, -0.1))
-#dev.off()
+dev.off()
 
 
 # GRAPHIQUE GINI CAPITAL - tot capital partir
 # Créer une palette de couleurs graduelle de bleu à rouge
 
-#png(filename = "../results/partie/init_egaux/VA_profnappe_Gini VA.png", width = 800, height = 600)
+png(filename = "../results/partie/irl/VA_profnappe_Gini VA.png", width = 800, height = 600)
 #png(filename = "../results/partie/VA_profnappe_Gini VA.png", width = 800, height = 600)
 
 color_palette <- colorRampPalette(c("blue", "red"))
@@ -178,27 +206,35 @@ plot(x = tabparties$sum_cap, y = tabparties$profnappe,
 par(xpd = TRUE) # Permet d'afficher la légende en dehors de la zone du graphique
 legend("topright", legend = seq(mincap, maxcap, length.out = 5), fill = color_palette(5), 
        title = "gini_capital", cex = 0.8, bty = "n", inset = c(0, -0.1))
-#dev.off()
+dev.off()
 
 
 
-####################### Analyse ACP
+### ANALYSE  acp#############
 library(dplyr)
 library(FactoMineR)
 library(factoextra)
-
+dev.off()
 res.pca <- PCA(tabparties[,c(-1)], scale.unit=TRUE, ncp=2, graph=T)
 
 
+png(filename = "../results/partie/irl/acp_var.png", width = 800, height = 600)
 
 fviz_pca_var(res.pca, col.var = "cos2", 
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE )
+dev.off()
+
+png(filename = "../results/partie/irl/acp_indiv.png", width = 800, height = 600)
+
 fviz_pca_ind(res.pca, axes = c(1,2), geom.ind = "point", 
              addEllipses = TRUE, legend.title = "idividu")
+dev.off()
 
 
-
-
+png(filename = "../results/partie/irl/acp_indiv_var.png", width = 800, height = 600)
+fviz_pca_biplot(res.pca, label ="var", col.ind="cos2") +
+       theme_minimal()
+dev.off()
 ###########################Essai joueurs
 
 tabparties_irl<-cbind(irl_gini,irl_captot$Capital_final)
